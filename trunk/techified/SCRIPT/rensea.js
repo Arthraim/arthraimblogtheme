449725724e279bda8597271cdd73e1cdaf09d1d8
arthraim <arthraim@c4f96794-2c48-11de-94d6-134fa40061eb>
2009-09-04 11:31:32 +0000

    2009-09-05. techfied. Modified rensea.js.

--- trunk/techified/SCRIPT/rensea.js
+++ trunk/techified/SCRIPT/rensea.js
@@ -7,32 +7,7 @@ function renseaCallback(statuss) {
     }).replace(/\B@([_a-z0-9]+)/ig, function(reply) {
       return  reply.charAt(0)+'<a href="http://rensea.com/'+reply.substring(1)+'">'+reply.substring(1)+'</a>';
     });
-    statusHTML.push('<li><span>'+status+'</span> <a style="font-size:85%" href="http://rensea.com/'+username+'/statuses/'+statuss[i].id+'">'+relative_time(statuss[i].created_at)+'</a></li>');
+    statusHTML.push('<li>'+status+'</li>');
   }
   document.getElementById('rensea_update_list').innerHTML = statusHTML.join('');
-}
-
-function relative_time(time_value) {
-  var values = time_value.split(" ");
-  time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
-  var parsed_date = Date.parse(time_value);
-  var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
-  var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
-  delta = delta + (relative_to.getTimezoneOffset() * 60);
-
-  if (delta < 60) {
-    return 'less than a minute ago';
-  } else if(delta < 120) {
-    return 'about a minute ago';
-  } else if(delta < (60*60)) {
-    return (parseInt(delta / 60)).toString() + ' minutes ago';
-  } else if(delta < (120*60)) {
-    return 'about an hour ago';
-  } else if(delta < (24*60*60)) {
-    return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';
-  } else if(delta < (48*60*60)) {
-    return '1 day ago';
-  } else {
-    return (parseInt(delta / 86400)).toString() + ' days ago';
-  }
 }
